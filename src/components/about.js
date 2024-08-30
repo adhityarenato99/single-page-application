b22f1d0976fd0728cfd3a33a985e5757f7a0f6a5
BINUS\adhitya.dwijayantho <adhitya.dwijayantho@binus.edu>
2024-08-31 01:38:41 +0700

    Do some modification in about and home

--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
 
-const About = () => {
+export const About = (props) => {
     return (
-        <>
-        <h1>About page</h1>
-        </>
-    )
-}
-
-export default About
+        <div id="team" className="text-center">
+            <div className="container">
+                <div className="col-md-8 col-md-offset-2 section-title">
+                    <h2>Meet the Team</h2>
+                    <p>
+                        Meet the team who run EdVentura!
+                    </p>
+                </div>
+                <div id="row">
+                    {props.data
+                        ? props.data.map((d, i) => (
+                            <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                                <div className="thumbnail">
+                                    {" "}
+                                    <img src={d.img} alt="..." className="team-img" />
+                                    <div className="caption">
+                                        <h4>{d.name}</h4>
+                                        <p>{d.job}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                        : "loading"}
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
-const Home = () => {
+export const Home = (props) => {
     return (
-        <>
-        <h1>Home page</h1>
-        </>
-    )
-}
-
-export default Home
+        <div id="header">
+            <div className="container">
+                <div className="row" style={{paddingTop:'80px'}}>
+                    <div className="col-xs-12 col-md-6">
+                        <div className="about-text" style={{ paddingTop: '50px' }}>
+                            <h2 style={{marginBottom: '40px'}}>Come and join us at EdVentura!, an amazing indoor interactive playcentre owned and operated
+                                by SENT Entertainment and supported by COMO 1907.</h2>
+                            <h3>EdVentura is the first indoor educational and interactive climbing playcentre space for 5 to 11
+                                years olds, set right in the centre of beautiful Como. It offers an aspirational and fun digital
+                                learning experience using state of the art audiovisual systems powered by a wide range of
+                                educational applications.</h3>
+                            {/* <div className="list-style">
+                                <p>Paragraph...</p>
+                            </div> */}
+                        </div>
+                    </div>
+                    <div className="col-xs-12 col-md-6">
+                        {" "}
+                        <img src="img/man-climbing-wall.jpg" className="img-responsive" alt="" fluid />{" "}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
