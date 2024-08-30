4da79bb1984d9c0b53ec5c28f6bcd475c151c3c4
BINUS\adhitya.dwijayantho <adhitya.dwijayantho@binus.edu>
2024-08-30 23:33:00 +0700

    Add initial 'Header' section

---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './header.css'
+
+export const Header = (props) => {
+    return (
+        <header id="header">
+            <div className="intro">
+                <div className="overlay">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-8 col-md-offset-2 intro-text">
+                                <a href="#header" className="btn btn-custom btn-lg page-scroll bottom-right-button">
+                                    Book Your Session
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
