fdb38e62e295cc19fc3da433ae8c6ade161b9e62
BINUS\adhitya.dwijayantho <adhitya.dwijayantho@binus.edu>
2024-08-31 02:30:40 +0700

    Add bookings page

--- src/components/bookings.js
+++ src/components/bookings.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import './bookings.css';
 
-const Bookings = () => {
+export const Bookings = () => {
     return (
-        <>
-        <h1>Bookings page</h1>
-        </>
-    )
-}
-
-export default Bookings
+        <div id="bookings" className="text-center">
+            <div className="container">
+                <div className="pricing-card">
+                    <div className="pricing-content">
+                        <p className="session-text">Per session</p>
+                        <h2 className="price">€20</h2>
+                        <p className="membership-text">Become a member with a yearly membership fee of</p>
+                        <h2 className="price">€120</h2>
+                        <a href="#book-session" className="btn btn-book">BOOK YOUR SESSION!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
