aba29e7b0de973370270b477372b5484024846af
BINUS\adhitya.dwijayantho <adhitya.dwijayantho@binus.edu>
2024-08-31 06:50:12 +0700

    Improve App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Navigation } from './components/components';
 import { Header } from './components/header';
+import { Home } from './components/home';
+import { Features } from './components/features';
+import { About } from './components/about';
+import { Activities } from './components/activities';
+import { ActivitiesAndSessions } from './components/activitiesSessions';
+import { Bookings } from './components/bookings';
+import { Footer } from './components/footer';
+
 import SmoothScroll from 'smooth-scroll';
+
+// Create dummy data
+import JsonData from "./data/data.json";
 
 import './App.css';
 
@@ -14,14 +25,21 @@ const App = () => {
 
   const [LandingPageData, setLandingPageData] = useState([]);
   useEffect(() => {
-    // console.log(JsonData)
-    // setLandingPageData(JsonData)
+    console.log(JsonData)
+    setLandingPageData(JsonData)
   },[])
 
   return (
     <div>
       <Navigation />
       <Header />
+      <Home />
+      <Features />
+      <About data={LandingPageData.Team}/>
+      <Activities />
+      <ActivitiesAndSessions />
+      <Bookings />
+      <Footer />
     </div>
   )
 
