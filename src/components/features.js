9350e2baa919ef9be0a3da9df671b3872b146133
BINUS\adhitya.dwijayantho <adhitya.dwijayantho@binus.edu>
2024-08-31 02:40:24 +0700

    change image

--- src/components/features.js
+++ src/components/features.js
@@ -7,7 +7,7 @@ export const Features = () => {
             <div className="row">
                 <div className="col-md-6 col-xs-12">
                     <div className="card feature-card blue-card">
-                        <img src="img/climbing.jpg" alt="LU WALL" className="card-img-top feature-image" fluid />
+                        <img src="img/climbing3.png" alt="LU WALL" className="card-img-top feature-image" fluid />
                         <div className="card-body">
                             <h3 className="card-title feature-title">THE AMAZING</h3>
                             <h1 className="card-text feature-heading">LU WALL</h1>
@@ -17,7 +17,7 @@ export const Features = () => {
                 </div>
                 <div className="col-md-6 col-xs-12">
                     <div className="card feature-card orange-card">
-                        <img src="img/climbing1.jpg" alt="CLIMBING WALL" className="card-img-top feature-image" fluid />
+                        <img src="img/climbing4.png" alt="CLIMBING WALL" className="card-img-top feature-image" fluid />
                         <div className="card-body">
                             <h3 className="card-title feature-title">Interactive Game</h3>
                             <h1 className="card-text feature-heading">CLIMBING WALL</h1>
